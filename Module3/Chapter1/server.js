642b223782eef1215bb6f7d6d4b0d66e4e76f389
OwenS3881 <owenski08@gmail.com>
2025-01-06 15:11:22 -0500

    Mod 3, Ch. 1

---
+++ Module3/Chapter1/server.js
@@ -0,0 +1,32 @@
+//console test
+console.log("Hello World!");
+
+//global object
+// console.log(global);
+
+//importing modules
+const os = require("os");
+const path = require("path");
+const { add, subtract, multiply, divide } = require("./math"); //custom module
+
+/*
+//os and path
+console.log(os.type());
+console.log(os.version());
+console.log(os.homedir());
+
+console.log(__dirname);
+console.log(__filename);
+
+console.log(path.dirname(__filename));
+console.log(path.basename(__filename));
+console.log(path.extname(__filename));
+
+console.log(path.parse(__filename));
+*/
+
+//custom modules
+console.log(add(2, 3));
+console.log(subtract(2, 3));
+console.log(multiply(2, 3));
+console.log(divide(2, 3));
